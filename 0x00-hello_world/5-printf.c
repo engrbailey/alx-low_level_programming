2034427787d67ca110196cd3087bd8a8f2a886f0
engrbailey <engrbailey@gmail.com>
2023-07-23 20:05:20 -0700

    Another one

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printf: you know what it is
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
