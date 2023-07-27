61ff6a294360e91ceb0e71ad735c67c76d9e47de
Bailey Emekoba <chidalub@gmail.com>
2023-07-27 17:03:27 +0100

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <main.h>
-
+#include "main.h"
 /**
  * main - Entry point
- * Return:0
- */
-
+ * Description: 'prints _putchar to stdout'
+ * Return: 0 means successful
+*/
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n')
+    return (0);
 }
